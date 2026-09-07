005b1e35be413884360ceebd7090506a199cd84c
rafe-designs <aminu4235@gmail.com>
2026-09-07 11:43:45 +0100

    new updates

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: "standalone",
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
